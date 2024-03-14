4479cbdea1ec7b4f0cb2cf8b1fa4af35bb3a00d6
Nida Fatima <2529nida17@gmail.com>
2024-03-14 16:49:40 +0530

    .

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,1 +1,12 @@
 alert("Hello World");
+console.log("code is running...")
+console.log("code is also running...")
+var a=  prompt("Enter Your Number ")
+var isTrue =confirm("Are you sure you want to leave this page and blast your computer")
+if(isTrue){
+    console.log("Computer is blasting")
+}
+else{
+    console.log("Computer is not  blasting")
+}
+console.log("Your number is "+ a)
