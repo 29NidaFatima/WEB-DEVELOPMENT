2b135e66895e453b28e9352de322ddfe596a4592
Nida Fatima <2529nida17@gmail.com>
2024-03-16 19:39:36 +0530

    .

---
+++ JavaScript 1/script.js
@@ -0,0 +1,14 @@
+console.log("Tutorial 55 of JS")
+var a=5;
+var b=6;
+var c="Nida"
+//var 55a="Rohan"    Invalid Way-not Allowed
+
+
+
+
+console.log(a+b+8)
+console.log(typeof a,typeof b, typeof c)
+
+const a1=29; //value cannot change if once assign in const
+a1 = a1 + 1;
