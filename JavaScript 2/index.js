b2fa1c3d7c7e1ae13dccf137277da72a3f911ff1
Nida Fatima <2529nida17@gmail.com>
2024-03-30 08:07:25 +0530

    .

--- JavaScript 2/index.js
+++ JavaScript 2/index.js
@@ -1,15 +1,57 @@
 console.log("Hello I am Conditional tutorial")
-let age=18;
-// let grace=2;
+// let age = 1;
+// let grace = 2;
+
 // console.log(age+grace)
 // console.log(age-grace)
 // console.log(age*grace)
 // console.log(age/grace)
-// console.log(age**grace)     //** means------>Exponential
+// console.log(age**grace)     //** means------>Exponentiation Operator
 // console.log(age%grace)
-if(age!=18){
-    console.log("You can Drive");
-}
-else{
-    console.log("You cannot Drive");
-}
+// if (age == 18) {
+//     console.log("You can Drive");
+// }
+// else if(age==0){
+//     console.log("Are You Kidding")
+// }
+// else if(age==1){
+//     console.log("Are You Again Kidding")
+// }
+// else {
+//     console.log("You cannot Drive");
+// }
+//TERNARY OPERATOR
+//  let a=6;
+// let b=8;
+// console.log( c= a>b?(a-b):(b-a));
+//ASSIGNMENT OPERATOR
+// age += grace
+// console.log(age)
+//---------------PRACTICE QUESTION--------------
+//Q1
+// let age= 21;
+// if(age>10 || age<20){
+//     console.log("Valid age")
+// }
+// else{
+// console.log("Invalid age")
+// }
+
+//Q2
+// let number= 12;
+// if(number%2==0){
+//     console.log("NUMBER IS DIVISIBLE BY 2")
+// }
+// else if(number%3==0){
+//     console.log("NUMBER IS DIVISIBLE BY 3")
+// }
+// else{
+//     console.log("NOT DIVISIBLE ")
+// }
+//Q3
+// if((number%2==0)&& (number%3==0)){
+//     console.log("DIVISIBLE BY 2 AND 3")
+// }
+// Q4
+let age=21;
+console.log(age>18? "You Can Drive": "You Cannot Drive")
