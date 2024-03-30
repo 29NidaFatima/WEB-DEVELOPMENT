7e558e13fe86a3ec31800ebbd50c1c00e5e2e22a
Nida Fatima <2529nida17@gmail.com>
2024-03-30 06:49:36 +0530

    .

--- JavaScript 1/script.js
+++ JavaScript 1/script.js
@@ -1,14 +1,38 @@
 console.log("Tutorial 55 of JS")
-var a=5;
-var b=6;
-var c="Nida"
+let a=5;
+let b=6;
+let c="Nida"
 //var 55a="Rohan"    Invalid Way-not Allowed
 
+{ 
+    // let -->BLOCK SCOPE
+    let b=66;
 
+    console.log(b)
+}
+console.log(b)
 
+// console.log(a+b+8)
+// console.log(typeof a,typeof b, typeof c)
 
-console.log(a+b+8)
-console.log(typeof a,typeof b, typeof c)
+// const a1=29; //value cannot change if once assign in const
+// a1 = a1 + 1;
 
-const a1=29; //value cannot change if once assign in const
-a1 = a1 + 1;
+let x="Nida Fatima";
+let y=22;
+let z=3.55;
+const p=true;
+let q=undefined;
+let r=null;
+console.log(x,y,z,p,q,r)
+console.log(typeof x,typeof y,typeof z,typeof p,typeof q,typeof r)
+let o={
+    "name": "Nida",
+    "job code": 5600, 
+    "is_handsome":True
+}
+console.log(o)
+o.salary="100crores"
+console.log(o)
+o.salary="500crores"
+console.log(o)
---
+++ JavaScript 2/index.js
@@ -0,0 +1,15 @@
+console.log("Hello I am Conditional tutorial")
+let age=18;
+// let grace=2;
+// console.log(age+grace)
+// console.log(age-grace)
+// console.log(age*grace)
+// console.log(age/grace)
+// console.log(age**grace)     //** means------>Exponential
+// console.log(age%grace)
+if(age!=18){
+    console.log("You can Drive");
+}
+else{
+    console.log("You cannot Drive");
+}
