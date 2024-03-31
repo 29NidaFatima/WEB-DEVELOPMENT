0bc403f802f2d58b8b34ae842e933ca4fd63aaaf
Nida Fatima <2529nida17@gmail.com>
2024-03-31 16:55:59 +0530

    .

---
+++ JavaScript 3/index.js
@@ -0,0 +1,35 @@
+console.log("I am a tutorial on Loops")
+let a=1;
+// console.log(a)
+// console.log(a+1)
+// console.log(a+2)
+// console.log(a+3)
+// console.log(a+4)
+// for (let i = 0; i < 100; i++) {
+  
+//     console.log(a+i);
+// }
+// let obj={
+//      name:"Nida",
+//      role:"Programmer",
+//      company:"CodeWithHarry AI"
+// }
+// for (const key in obj) {
+
+//         // const element = obj[key]; 
+//         console.log( key)
+        
+//     }
+//     for (const char of "Nida") {
+//         console.log(char)
+//     }
+// let i=0;
+// while (i<6) {
+//     console.log(i)
+//     i++;
+// }
+let i=10;
+do {
+   console.log(i);
+   i++;
+} while (i<6);
---
+++ JavaScript 4/index.js
@@ -0,0 +1,23 @@
+
+// function nice(name){
+//     console.log("Hey "  +name+  " you are nice!") 
+//     console.log("Hey " +name+ " you are good") 
+// }
+// nice("Nida")
+// nice("Fatima")
+function sum(a,b,c=4){
+    // console.log(a+b)
+    console.log(a,b,c)
+    return a+b+c
+}
+ result1=sum(2)
+ result2=sum(2,8)
+ result3=sum(12,14,1)
+ console.log("The sum of these numbers is: ",result1)
+ console.log("The sum of these numbers is: ",result2)
+ console.log("The sum of these numbers is: ",result3)
+ const func1= (x)=>{
+    console.log("I am arrow function",x)
+ }
+ func1(34);
+ func1(66);
