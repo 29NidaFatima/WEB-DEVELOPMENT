0ae5ea035efc600a7c18afcb5a385f8d9194ba02
NidaFatima <2529nida17@gmail.com>
2024-05-26 12:55:13 +0530

    .

---
+++ JS/BOX COLOR USING DOM/index.js
@@ -0,0 +1,12 @@
+let boxes= document.body.getElementsByClassName("box");
+console.log(boxes)
+boxes[0].style.color="pink"
+boxes[1].style.color="yellow"
+boxes[2].style.color="green"
+boxes[3].style.color="blue"
+boxes[4].style.color="red"
+boxes[0].style.backgroundColor="yellow"
+boxes[1].style.backgroundColor="blue"
+boxes[2].style.backgroundColor="red"
+boxes[3].style.backgroundColor="pink"
+boxes[4].style.backgroundColor="black"
