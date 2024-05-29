7b0381f9ba287cf4b1de84b0e03ae7dd8dce6538
NidaFatima <2529nida17@gmail.com>
2024-05-29 12:49:36 +0530

    .

--- JS/Dynamic Website builder/script.js
+++ JS/Dynamic Website builder/script.js
@@ -1,25 +1,33 @@
+
 function createCard(title, cName, views, monthsOld, duration, thumbnail) {
-    let viewStr;
-    if(views<1000000){
-        let viewStr=views/1000+"K"
+    // Finish this function
+    let viewStr
+    if (views < 1000) {
+        viewStr = views;
     }
-    else if(views>100000){
-        let viewStr=views/1000000+"M"
+    else if (views > 1000000) {
+        viewStr = views / 1000000 + "M";
     }
-    else{
-let viewStr=views/1000+"K";
+    else {
+        viewStr = views / 1000 + "K";
     }
-   
- let html=`<div class="card">
- <div class="image">
-     <img src="${thumbnail}">
- </div>
- <div class="capsule"></div>
- <div class="text">
-     <h1>${title}</h1>
-     <p>${cName} . ${viewStr} views. ${monthsOld}months ago</p>
- </div>
+    let html = `<div class="card">
+    <div class="image">
+        <img src="${thumbnail}"
+            alt="">
+        <div class="capsule">${duration}</div>
+    </div>
+    <div class="text">
+        <h1>${title}</h1>
+        <p>${cName} . ${viewStr} views . ${monthsOld} months ago</p>
+    </div> 
 </div>`
 
+    document.querySelector(".container").innerHTML = document.querySelector(".container").innerHTML + html
 }
-createCard("Installing VS Code & How Websites Work | Sigma Web Development Course - Tutorial #1", "CodeWithHarry", 1700000, "31:20", "imagee.webp")
+//FunctionCall
+createCard("Introduction to CSS | Sigma Web Development Course - Tutorial #1", "CodeWithHarry", 999, 8, "32:00", "imagee.webp")
+
+createCard("Introduction to Express.JS | Sigma Web Development Course - Tutorial #2", "CodeWithHarry", 17000000, 9, "32:00", "imagee.webp")
+
+createCard("Introduction to REACT | Sigma Web Development Course - Tutorial #3", "CodeWithHarry", 180000, 10, "35:10", "imagee.webp")
