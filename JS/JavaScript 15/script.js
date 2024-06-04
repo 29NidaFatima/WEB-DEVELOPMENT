6377178349ee6f04004f6ed494e0aaaa55b9180c
NidaFatima <2529nida17@gmail.com>
2024-06-04 14:10:35 +0530

    .

--- JS/JavaScript 15/script.js
+++ JS/JavaScript 15/script.js
@@ -13,31 +13,77 @@
 // }
 // rabbit._proto_=animal //set rabbit.[[Prototype]]=animal
 // --------------CLASSES & Constructor----------------
-class Animal{
+// class Animal{
 
-    constructor(name){
-        this.name=name
-        console.log("object is created");
-    }
-    eats(){
-        console.log("Eating");
-    }
-    jumps(){
-        console.log("Jumping");
-    }
-}
-let a = new Animal("Bunny");
-console.log(a);
-class Lion extends Animal{
-    constructor(name){
-        super(name)
+//     constructor(name){
+//         this.name=name
+//         console.log("object is created");
+//     }
+//     eats(){
+//         console.log("Eating");
+//     }
+    // jumps(){
+//         console.log("Jumping");
+//     }
+// }
+// let a = new Animal("Bunny");
+// console.log(a);
+// class Lion extends Animal{
+//     constructor(name){
+//         super(name)
       
-        console.log("object is created and he is lion...");
+//         console.log("object is created and he is lion...");
+//     }
+// eats(){
+//     super.eats()
+//             console.log("Eating &roar");
+//         }
+// }
+// let l =new Lion("Shera")
+// console.log(l);
+// -----------------practice set---------------
+// Q1
+// class Complexnumber{
+// constructor(realpart, complexpart){
+// this.realpart=realpart;
+// this.complexpart=complexpart;
+// }
+
+// }
+// let obj1= new Complexnumber(12,3);
+// let obj2= new Complexnumber(1,4);
+
+// // Q2
+class Complexnumber{
+    constructor(realpart, imaginarypart){
+    this.realpart=realpart;
+    this.complexpart=imaginarypart;
     }
-eats(){
-    super.eats()
-            console.log("Eating &roar");
-        }
+    add(num){
+let r= this.realpart+num.realpart;
+let q=this.imaginarypart+num.imaginarypart
+ return new Complexnumber(r,i)
+    }
+    }
+    let obj1= new Complexnumber(12,3);
+    let obj2= new Complexnumber(1,4);
+    obj1.add(obj2)
+    console.log(obj1.realpart,obj2.imaginarypart);
+    // Q3
+    class Human{
+study(){
+    console.log("learn all subject")
 }
-let l =new Lion("Shera")
-console.log(l);
+
+    }
+    class Student extends Human{
+study(){
+    super().study
+console.log("learn a particular subject");
+}
+    }
+    let obj= new Student();
+    // // Q4
+    let check= Student instanceof Human;
+    console.log(check);
+
