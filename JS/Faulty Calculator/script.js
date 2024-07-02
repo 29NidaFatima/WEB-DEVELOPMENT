4ae44d9f5f50c03efd6d35f84c123ee00651c99a
NidaFatima <2529nida17@gmail.com>
2024-07-02 17:56:02 +0530

    .

--- JS/Faulty Calculator/script.js
+++ JS/Faulty Calculator/script.js
@@ -1,25 +1,4 @@
-// let random=Math.random()
-// console.log(random)
-// let a= prompt("Enter first number")
-// let c=prompt("Enter operation")
-// let b= prompt("Enter second number")
-
-// let obj={
-//     "+":"-",
-//     "*":"+",
-//     "-":"/",
-//     "/":"**"
-// }
-
-// if(random>0.1){
-//     console.log(`The Result is ${a} ${c} ${b}`);
-//     alert(`The Result is ${eval('${a} ${c} ${b}')}`);
-// }
-// else{
-//     c=obj[c]
-//     alert(`The Result is ${eval('${a} ${c} ${b}')}`);
-// }
-// ---------------------------------
+console.log("FAULTY CALCULATOR");
 let random = Math.random();
 console.log(random);
 
@@ -32,11 +11,11 @@ let obj = {
     "*": "+",
     "-": "/",
     "/": "**"
-};
-
+}
 if (random > 0.1) {
-    let result = eval(`${a} ${c} ${b}`);
     console.log(`The Result is ${a} ${c} ${b}`);
+    let result = eval(`${a} ${c} ${b}`);
+   
     alert(`The Result is ${result}`);
 } else {
     c = obj[c];
