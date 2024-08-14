8f9eabe9ea1ea9a0e024a32fbb5131a8d927df43
NidaFatima <2529nida17@gmail.com>
2024-08-14 07:26:04 +0530

    .

--- JS/JavaScript 7/question.js
+++ JS/JavaScript 7/question.js
@@ -1,46 +1,37 @@
 // Q1
-// let age=prompt("Enter Your Age");
-// age=Number.parseInt(age)
-// if (age>18) {
-//     alert("You Can drive")
-// } 
+let age=prompt("Enter Your Age");
+age=Number.parseInt(age)
+if (age>18) {
+    alert("You Can drive")
+} 
 
-// else {
-//   alert("You Cannot drive")  
-// }
+else {
+  alert("You Cannot drive")  
+}
 // Q2
-// confirm("Do You Want to again drive")
-// let age=prompt("Enter Your Age");
-// age=Number.parseInt(age)
-// if (age>18) {
-//     alert("You Can drive")
-// } 
+confirm("Do You Want to again drive")
+let age=prompt("Enter Your Age");
+age=Number.parseInt(age)
+if (age>18) {
+    alert("You Can drive")
+} 
 
-// else {
-//   alert("You Cannot drive")  
-// }
+else {
+  alert("You Cannot drive")  
+}
 //Q3
-// let age=prompt("Enter Your Age");
-// age=Number.parseInt(age)
-// if(age<0) {
-//   console.error("Enter a valid no.");
- 
-// }
-//   else if (age>18) {
-//     alert("You Can drive")
-// } 
+let age=prompt("Enter Your Age");
+age=Number.parseInt(age)
+if(age<0) {
+  console.error("Enter a valid no.");
+}
 
-// else {
-//   alert("You Cannot drive")  
-// }
 // Q4
 let number= prompt("Enter a Number")
 number=Number.parseInt(number)
 if(number>4) {
   location.href ="https://myntra.com"
 }
-//Q5
-
-
-// let color=prompt("Enter a background color")
-// document.body.style.backgroundColor = color
+// Q5
+let color=prompt("Enter a background color")
+document.body.style.backgroundColor = color
