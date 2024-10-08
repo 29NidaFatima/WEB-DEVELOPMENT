d62b8da238f64ff511026f97d834534e4e5215e7
NidaFatima <2529nida17@gmail.com>
2024-10-08 21:35:07 +0530

    .

--- JS/JavaScript 8/script.js
+++ JS/JavaScript 8/script.js
@@ -1,14 +1,14 @@
-// Q1
+// // Q1
 // console.log("Hello  world")
-let cont = document.body.firstElementChild
-cont.children[0].style.color="red"
+// let cont = document.body.firstElementChild
+// cont.children[0].style.color="red"
 
 
-// Q3
+// // Q3
 
-document.body.firstElementChild.children[0].style.color="green"
-document.body.firstElementChild.children[2].style.color="green"
-// Q2
+// document.body.firstElementChild.children[0].style.color="green"
+// document.body.firstElementChild.children[2].style.color="green"
+
 Array.from(document.getElementsByTagName("li")).forEach(element => {
     element.style.background="cyan";
 });
--- JS/JavaScript 9/script.js
+++ JS/JavaScript 9/script.js
@@ -1,9 +1,10 @@
-// console.log("Nida")
-// let boxes= document.getElementsByClassName("box");
-// console.log(boxes)
-// boxes[2].style.backgroundColor="blue"
-// document.getElementById("redbox").style.backgroundColor="pink"
-// document.querySelector(".box").style.backgroundColor="yellow" //For a Particular starting single box whose  class name matches
+console.log("Nida")
+let boxes= document.getElementsByClassName("box");
+console.log(boxes);
+boxes[2].style.backgroundColor="blue"
+document.getElementById("redbox").style.backgroundColor="pink"
+document.querySelector(".box").style.backgroundColor="yellow" 
+
 console.log(document.querySelectorAll(".box"))
 document.querySelectorAll(".box").forEach(element => {
     element.style.backgroundColor="green"
