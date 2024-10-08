64185eb9fa9fcf2b2a00bee1cb44ede720496a15
NidaFatima <2529nida17@gmail.com>
2024-10-08 21:42:58 +0530

    .

--- Project/Snow Resort Website/script.js
+++
@@ -1,9 +0,0 @@
-
-document.addEventListener("DOMContentLoaded", function() {
-    var loginBtn = document.getElementById("login-btn");
-    var loginForm = document.querySelector(".login-form");
-
-    loginBtn.addEventListener("click", function() {
-        loginForm.classList.toggle("active");
-    });
-});
